d8a28697591d1818633f1559644ae3b354feb4e8
Chetan2028 <bmchetan2028@gmail.com>
2020-10-26 07:45:02 +0530

    Add project files.

---
+++ ExampleOfNLog/AddNumbers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleOfNLog
+{
+    class AddNumbers
+    {
+
+        Nlog nlog = new Nlog();
+
+        public int Add(int firstNumber , int secondNumber)
+        {
+            if (firstNumber == 0 || secondNumber == 0)
+            {
+                nlog.LogDebug("Debug Unsuccessful : Add()");
+                nlog.LogError("Values enetered  by user should shouold be greater than zero");
+                nlog.LogWarn("First or second number should be greater than zero");
+            }
+            else
+            {
+                int sum = firstNumber + secondNumber;
+                nlog.LogDebug("Debug Successfull : Add()");
+                nlog.LogInfo("Add() : passed");
+                return sum;
+            }
+            return 0;
+          
+        }
+    }
+}
---
+++ ExampleOfNLog/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ExampleOfNLog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter two numbers");
+            int firstNumber = Convert.ToInt32(Console.ReadLine());
+            int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+            AddNumbers addNumbers = new AddNumbers();
+            int sum = addNumbers.Add(firstNumber, secondNumber);
+            Console.WriteLine("sum is : " + sum);
+        }
+    }
+}
